912c4b11c9f24b22a0da807f8d7e042cc212ec98
Ahmed <ahmedabozaid.dev@gmail.com>
2025-03-25 16:54:26 +0200

    R - Removes all null values

--- R/test.R
+++ R/test.R
@@ -1,1 +1,8 @@
 dataset <- read.csv('../Data.csv')
+
+# removes all null values from age
+dataset$Age <- ifelse(is.na(dataset$Age),
+                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                      dataset$Age)
+
+# removes all null values from Salary
