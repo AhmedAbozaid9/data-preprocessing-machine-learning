94160b2b41796d6ba1c6a8c04156c7d5e75acff6
Ahmed <ahmedabozaid.dev@gmail.com>
2025-03-25 16:59:42 +0200

    R - Removes all null values from salary

--- R/test.R
+++ R/test.R
@@ -6,3 +6,8 @@ dataset$Age <- ifelse(is.na(dataset$Age),
                       dataset$Age)
 
 # removes all null values from Salary
+dataset$Salary <- ifelse(is.na(dataset$Salary),
+                         ave(dataset$Age, Fun = function(x) mean(x, na.rm = TRUE)),
+                         dataset$Age)
+# another approach
+dataset$Salary <- replace(dataset$Salary, is.na(dataset$Salary), mean(dataset$Salary, na.rm = TRUE))
